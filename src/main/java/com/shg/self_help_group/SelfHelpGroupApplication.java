4db9595a39d14ad1d196af4cb70462cd048184e2
HariPad2005 <hariharanbindu2019@gmail.com>
2025-01-27 11:46:58 +0530

    Initial Changes

---
+++ src/main/java/com/shg/self_help_group/SelfHelpGroupApplication.java
@@ -0,0 +1,13 @@
+package com.shg.self_help_group;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfHelpGroupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelfHelpGroupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shg/self_help_group/SelfHelpGroupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shg.self_help_group;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfHelpGroupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
